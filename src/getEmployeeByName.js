eabf0b52d2da42832e3556de0141c943ea7c1ccf
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-07-28 17:23:57 -0300

    function getEmployeebyName foi implementada

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees
+    .find((employ) => employ.firstName === employeeName || employ.lastName === employeeName);
 }
 
+console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;
