f4b3b9d64b3df0e6f361b5482bf1728c72faf39a
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-07-28 17:00:27 -0300

    function getAnimalsOlderThan foi implementada

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const encontraEspecies = species.find((animais) => animais.name === animal);
+  return encontraEspecies.residents.every((animals) => animals.age >= age);
 }
+
+console.log(getAnimalsOlderThan('lions', 12));
 
 module.exports = getAnimalsOlderThan;
