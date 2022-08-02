19efa4cc9c942a3a3b2cb5675c8ecc0dd1fdfb3c
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-02 15:31:02 -0300

    function getEmployeesCoverage implementada

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const personAndId = employees.map((obj) => {
+  const arr = {
+    id: obj.id,
+    fullName: `${obj.firstName} ${obj.lastName}`,
+    species: obj.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).name),
+    locations: obj.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).location),
+  };
+  return arr;
+});
+
+function getEmployeesCoverage(param) {
+  if (!param) {
+    return personAndId;
+  }
+
+  console.log(getEmployeesCoverage());
+
+  const validate = personAndId
+    .find((e) => e.fullName.includes(Object.values(param))
+      || e.id.includes(Object.values(param)));
+  if (!validate) {
+    throw new Error('Informações inválidas');
+  }
+  return validate;
 }
 
 module.exports = getEmployeesCoverage;
